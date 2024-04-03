2a232ab7c6a44213d00e2011a5a779cc2ed65038
David Lee <david.lee@defra.gov.uk>
2024-04-03 08:35:40 +0100

    fuel price charts complete

--- theme_3/3_1_4_fuel_prices_mfg_fsi.R
+++ theme_3/3_1_4_fuel_prices_mfg_fsi.R
@@ -13,6 +13,8 @@ source(here("utils", "load-font.R"))
 #   price for fuels used in manufacturing
 #   gas and electricity prices in the non-domestic sector
 # Both available at https://www.gov.uk/government/collections/industrial-energy-prices
+# Using here the figures excluding Climate Change Levy since that seems to be
+# what DESNZ do in their publications
 
 # Manufacturing fuel prices-----------------------------------------------------
 
@@ -44,10 +46,46 @@ mfg_fuel <- mfg_fuel |>
 
 
 
-mfg_fuel |> 
+mfg_chart <- mfg_fuel |> 
   ggplot() +
   geom_line(aes(x = date, y = value, colour = fuel), lwd = 1) +
   scale_colour_manual(values = af_colours()) +
   labs(x = NULL,
        y = "pence per kilowatt hour") +
   theme_ukfsr(base_family = "GDS Transport Website")
+
+save_graphic(mfg_chart, "3.1.4b", "fuel prices for manufacturing")
+
+
+# Non-domestic prices ----------------------------------------------------------
+# Available here: https://www.gov.uk/government/statistical-data-sets/gas-and-electricity-prices-in-the-non-domestic-sector
+# Using table 3.4.1
+
+ndcolspec <- cols(
+  year = col_double(),
+  quarter = col_double(),
+  electricity = col_double(),
+  gas = col_double()
+)
+
+nd_fuel <- aws.s3::s3read_using(FUN = read_csv,
+                                 bucket = ukfsr::s3_bucket(),
+                                 object = "theme_3/t3_1_4/output/csv/3_1_4c_non_domestic_fuel_prices.csv",
+                                 col_types = ndcolspec)
+
+nd_fuel <- nd_fuel |> 
+  pivot_longer(cols = c(-year, -quarter), names_to = "fuel") |> 
+  mutate(date = as.Date(paste0(year, "-", ((quarter - 1) * 3 + 1), "-01")),
+         fuel = factor(fuel,
+                       levels = c("electricity", "gas"),
+                       labels = c("Electricity", "Gas")))
+
+nd_chart <- nd_fuel |> 
+  ggplot() +
+  geom_line(aes(x = date, y = value, colour = fuel), lwd = 1) +
+  scale_colour_manual(values = af_colours()) +
+  labs(x = NULL,
+       y = "pence per kilowatt hour") +
+  theme_ukfsr(base_family = "GDS Transport Website")
+
+save_graphic(nd_chart, "3.1.4c", "non domestic fuel prices")
