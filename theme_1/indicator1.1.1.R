132411e8d3b5fa1238cfc9f1c7a9a7af71b9ee3e
JSmithSCE <jonathan.smith@apha.gov.uk>
2024-04-12 16:09:25 +0100

    first draft UKFSI graphics

---
+++ theme_1/indicator1.1.1.R
@@ -0,0 +1,62 @@
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+library(aws.ec2metadata)
+library(aws.s3)
+library(readr)
+library(ukfsr)
+library(afcolours)
+library(here)
+
+source(here("utils", "load-font.R"))
+
+kcalpp_temp <- aws.s3::s3read_using(FUN = read_csv,
+                            bucket = ukfsr::s3_bucket(),
+                            object = "theme_1/t1_1_1/output/csv/kcal_capita_day.csv")%>%
+                            filter(Year<2010)
+
+kcalppns <- aws.s3::s3read_using(FUN = read_csv,
+                            bucket = ukfsr::s3_bucket(),
+                            object = "theme_1/t1_1_1/output/csv/kcal_capita_day_nd.csv")
+
+kcalpp<-rbind(kcalpp_temp,kcalppns)
+
+kcalpp_chart <- kcalpp |> 
+  rename(year=Year) |>
+  rename(value=Value) |> 
+  rename(element=Element) |> 
+  select(year,value,element)  |>
+  #filter(year>2010)|>
+  ggplot() +
+  geom_line(aes(x = year, y = value, colour = element), lwd = 1) +
+  geom_vline(xintercept =2014,linetype="dashed")+
+  geom_text(aes(x=2009,y=2500,label="change in\ncals/capita/day\nmethodology"),size=4)+
+  scale_y_continuous(limits = c(2000,3000)) +
+  scale_colour_manual(values = af_colours("duo")) +
+  theme_ukfsr(base_family = "GDS Transport Website") +
+  labs(x = NULL,
+       y = "kcals/capita/day")
+
+
+save_graphic(kcalpp_chart, "1.1.1", "global food supply")
+
+production_index <- aws.s3::s3read_using(FUN = read_csv,
+                            bucket = ukfsr::s3_bucket(),
+                            object = "theme_1/t1_1_1/output/csv/Gross_per_capita_Production_Index.csv")
+
+production_index_chart <- production_index |> 
+  rename(year=Year) |>
+  rename(value=Value) |> 
+  rename(element=Element) |>
+  mutate(element="Gross per capita production") |>
+  select(year,value,element)  |>
+  #filter(year>2011)|>
+  ggplot() +
+  geom_line(aes(x = year, y = value, colour = element), lwd = 1) +
+  scale_y_continuous(limits = c(60,110)) +
+  scale_colour_manual(values = af_colours("duo")) +
+  theme_ukfsr(base_family = "GDS Transport Website") +
+  labs(x = NULL,
+       y = "production index number (2014-2016=100)")
+
+save_graphic(production_index_chart, "1.1.1", "global food production")
