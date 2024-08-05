bd5761c85b924f80642a243c8aea42939169381d
Sarath Kizhakkoott <sarathkizhakkoott@ranch-573.int.sce.network>
2024-08-05 16:22:41 +0100

    New support graph for household food security status by age, ethnicity and disability

--- theme_4/4.1.1 Household Food Security Status.R
+++ theme_4/4.1.1 Household Food Security Status.R
@@ -127,6 +127,150 @@ save_graphic(FSR_4_1_1a_plot, '4.1.1a','% of households food (Insecure)')
 #ggsave(here("data", "FSR_4_1_1a_pct_hh_insecure_map.png"), width = 960, height = 640, units = "px", dpi = 72)
 
 
+-----------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+FSR_4_1_1b <- aws.s3::s3read_using(FUN = readr::read_csv,
+                                   bucket = "s3-ranch-054",
+                                   object = "theme_4/input_data/Household food security by diability.csv")
 
 
 
+FSR_4_1_1b <- FSR_4_1_1b %>%
+  #gather(variable,value, `High`,`Marginal`,`Low`,`Very low`)
+  gather(variable,value, `Very low`,`Low`,`Marginal`,`High`)
+
+
+check_sum<-FSR_4_1_1b %>% 
+  #arrange(Year) %>% 
+  select(-variable) %>%
+  group_by(Disability) %>%
+  summarise(sum =sum(value)) #check sums add to 100
+
+
+# Turn question column into a character vector
+FSR_4_1_1b$variable <- as.character(FSR_4_1_1b$variable)
+# Then turn it back into a factor with the levels in the correct order
+FSR_4_1_1b$variable <- factor(FSR_4_1_1b$variable, levels=unique(FSR_4_1_1b$variable))
+
+
+source(here::here("utils", "load-font.R"))
+
+FSR_4_1_1b_plot <- ggplot(FSR_4_1_1b, aes(x = Disability, y = value, fill = variable, label = round(value,0))) +
+  geom_bar(stat = "identity", width = 0.5) +
+  geom_text(position = position_stack(vjust = 0.5), 
+            colour= "white",
+            family = "GDS Transport Website",
+            size = 10) +
+  scale_fill_manual(values=af_colours(), guide = guide_legend(reverse = TRUE)) +
+  labs(y = "Percentage household food security", x = NULL)+
+  coord_flip() +
+  # guides(colour=guide_legend(override.aes=list(size=1),reverse = TRUE)) +
+  theme_ukfsr(base_family = "GDS Transport Website",
+              horizontal = TRUE) +
+  theme(legend.position = "bottom", legend.title = element_blank())
+
+
+FSR_4_1_1b_plot
+
+save_graphic(FSR_4_1_1b_plot, '4.1.1b','Household food security status by disability') + 
+  save_csv(FSR_4_1_1b, '4.1.1b','Household food security status by disability')
+
+-----------------------------------------------------------------------------------------------------------------------------------------------------
+  
+  
+FSR_4_1_1c <- aws.s3::s3read_using(FUN = readr::read_csv,
+                                     bucket = "s3-ranch-054",
+                                     object = "theme_4/input_data/Household food security by age normal.csv")
+
+
+
+FSR_4_1_1c <- FSR_4_1_1c %>%
+  #gather(variable,value, `High`,`Marginal`,`Low`,`Very low`)
+  gather(variable,value, `Very low`,`Low`,`Marginal`,`High`)
+
+
+check_sum<-FSR_4_1_1c %>% 
+  #arrange(Year) %>% 
+  select(-variable) %>%
+  group_by(Age) %>%
+  summarise(sum =sum(value)) #check sums add to 100
+
+
+# Turn question column into a character vector
+FSR_4_1_1c$variable <- as.character(FSR_4_1_1c$variable)
+# Then turn it back into a factor with the levels in the correct order
+FSR_4_1_1c$variable <- factor(FSR_4_1_1c$variable, levels=unique(FSR_4_1_1c$variable))
+
+
+source(here::here("utils", "load-font.R"))
+
+FSR_4_1_1c_plot <- ggplot(FSR_4_1_1c, aes(x = Age, y = value, fill = variable, label = round(value,0))) +
+  geom_bar(stat = "identity", width = 0.5) +
+  geom_text(position = position_stack(vjust = 0.5), 
+            colour= "white",
+            family = "GDS Transport Website",
+            size = 10) +
+  scale_fill_manual(values=af_colours(), guide = guide_legend(reverse = TRUE)) +
+  labs(y = "Percentage household food security", x = NULL)+
+  coord_flip() +
+  # guides(colour=guide_legend(override.aes=list(size=1),reverse = TRUE)) +
+  theme_ukfsr(base_family = "GDS Transport Website",
+              horizontal = TRUE) +
+  theme(legend.position = "bottom", legend.title = element_blank())
+
+
+FSR_4_1_1c_plot
+
+save_graphic(FSR_4_1_1c_plot, '4.1.1c','Household food security status by age') + 
+  save_csv(FSR_4_1_1c, '4.1.1c','Household food security status by age')
+
+
+-----------------------------------------------------------------------------------------------------------------------------------------------------
+  
+  
+FSR_4_1_1d <- aws.s3::s3read_using(FUN = readr::read_csv,
+                                     bucket = "s3-ranch-054",
+                                     object = "theme_4/input_data/Household food security by ethinic group.csv")
+
+
+
+FSR_4_1_1d <- FSR_4_1_1d %>%
+  #gather(variable,value, `High`,`Marginal`,`Low`,`Very low`)
+  gather(variable,value, `Very low`,`Low`,`Marginal`,`High`)
+
+
+check_sum<-FSR_4_1_1d %>% 
+  #arrange(Year) %>% 
+  select(-variable) %>%
+  group_by(`Ethnic group`) %>%
+  summarise(sum =sum(value)) #check sums add to 100
+
+
+# Turn question column into a character vector
+FSR_4_1_1d$variable <- as.character(FSR_4_1_1d$variable)
+# Then turn it back into a factor with the levels in the correct order
+FSR_4_1_1d$variable <- factor(FSR_4_1_1d$variable, levels=unique(FSR_4_1_1d$variable))
+
+
+source(here::here("utils", "load-font.R"))
+
+FSR_4_1_1d_plot <- ggplot(FSR_4_1_1d, aes(x = `Ethnic group`, y = value, fill = variable, label = round(value,0))) +
+  geom_bar(stat = "identity", width = 0.5) +
+  geom_text(position = position_stack(vjust = 0.5), 
+            colour= "white",
+            family = "GDS Transport Website",
+            size = 10) +
+  scale_fill_manual(values=af_colours(), guide = guide_legend(reverse = TRUE)) +
+  labs(y = "Percentage household food security", x = NULL)+
+  coord_flip() +
+  # guides(colour=guide_legend(override.aes=list(size=1),reverse = TRUE)) +
+  theme_ukfsr(base_family = "GDS Transport Website",
+              horizontal = TRUE) +
+  theme(legend.position = "bottom", legend.title = element_blank())
+
+
+FSR_4_1_1d_plot
+
+save_graphic(FSR_4_1_1d_plot, '4.1.1c','Household food security status by ethnicity') + 
+  save_csv(FSR_4_1_1d, '4.1.1c','Household food security status by ethnicity')
