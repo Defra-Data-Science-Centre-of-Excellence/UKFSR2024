dc6b11b0dfa729546325c11079c26b7742aae2a4
David Lee <david.lee@defra.gov.uk>
2024-08-06 09:45:46 +0100

    Using ONS rapid.spreadsheets package to generate xlsx files

--- utils/s3-helpers.R
+++ utils/s3-helpers.R
@@ -39,3 +39,39 @@ purrr::map2(path, title, \(path, title) {
   write_ods(x, path = "~/work/test.ods", sheet = title, append = TRUE)
 }
 )
+
+# Experimenting with 'rapid.spreadsheets' and xlsx ----------------------------- 
+library(rapid.spreadsheets)
+library(openxlsx)
+library(ukfsr)
+library(dplyr)
+library(readODS)
+library(stringr)
+library(readr)
+
+csvs <- bucket_manifest(file_ext = "csv") |> 
+  dplyr::filter(stringr::str_starts(folder, "theme_1") & stringr::str_ends(folder, "output/csv")) |> 
+  mutate(sheet_name = str_replace_all(paste(row_number(), str_sub(title, 1, 28)), " ", "_"))
+
+path <- csvs$path
+title <- csvs$title
+sheet_names <- csvs$sheet_name
+
+data <- purrr::map(path, \(path) {
+  x <- aws.s3::s3read_using(FUN = read_csv,
+                            bucket = ukfsr::s3_bucket(),
+                            object = path)
+})
+
+names(data) <- sheet_names
+
+wb <- openxlsx::createWorkbook()
+purrr::pmap(list(data, sheet_names, title), \(data, sheet_names, title) {
+  create_data_table_tab(wb, data,tab_name = sheet_names, heading = title)
+  
+})
+
+contents <- tibble(sheet_names, title, desc = "link")
+create_contents_notes(wb, contents)
+
+saveWorkbook(wb, "~/test.xlsx", overwrite = TRUE)
