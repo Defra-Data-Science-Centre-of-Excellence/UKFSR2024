471fdd53d2c0f5e701ce2593be7a3c2bcfaf63eb
David Lee <david.lee@defra.gov.uk>
2024-10-22 14:23:15 +0100

    minor chart tweaks

--- theme_3/3.1.1 Agriculture Input Dependency.R
+++ theme_3/3.1.1 Agriculture Input Dependency.R
@@ -52,13 +52,18 @@ save_csv(FSR_3_1_1, '3.1.1a', ' UK principal farm costs')
 
 # Fertilizer Usage -------------------------------------------------------------
   
+
+# Data is from table AA1.7 in the British survey of fertiliser practice
+# https://www.gov.uk/government/statistical-data-sets/british-survey-of-fertiliser-practice-dataset
+
 FSR_3_1_1a <- aws.s3::s3read_using(FUN = readr::read_csv,
                                     bucket = "s3-ranch-054",
                                     object = "theme_3/input_data/Fertiliserusage.csv")
 
 FSR_3_1_1a <- FSR_3_1_1a %>%
   gather(variable,value, 'Nitrogen (kt)','Phosphate (kt)','Potash (kt)') %>%
-  mutate("Year" = as.Date(paste0(Year, "-01-01")))
+  mutate("Year" = as.Date(paste0(Year, "-01-01"))) |> 
+  filter(Year >= "1990-01-01")
 
 
 FSR_3_1_1aplot <- ggplot(FSR_3_1_1a, aes(x=Year, y=value, colour=variable, group=variable)) +
@@ -67,7 +72,7 @@ FSR_3_1_1aplot <- ggplot(FSR_3_1_1a, aes(x=Year, y=value, colour=variable, group
   guides(fill = guide_legend(byrow = TRUE)) +
   labs(x = NULL,
        y = "Thousand tonnes") +
-  scale_x_date(breaks=seq(as.Date("2003-01-01"),Sys.Date()-lubridate::years(1),by = "2 year"),date_labels = "%Y") +
+  scale_x_date(breaks=seq(as.Date("1990-01-01"),Sys.Date()-lubridate::years(1),by = "5 year"),date_labels = "%Y") +
   #scale_x_date(date_breaks = "12 months", date_labels = "%Y") +
   theme_ukfsr(base_family = "GDS Transport Website") +
   theme(plot.margin = margin(5,50,5,5,unit = "pt"))
