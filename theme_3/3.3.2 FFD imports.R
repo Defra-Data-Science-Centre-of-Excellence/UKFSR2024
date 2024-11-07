4993b13b1f5e59264978612222082161a193a007
David Lee <david.lee@defra.gov.uk>
2024-11-07 14:34:31 +0000

    added import volume chart

--- theme_3/3.3.2 FFD imports.R
+++ theme_3/3.3.2 FFD imports.R
@@ -9,7 +9,27 @@ library(zoo)
 
 source(here::here("utils", "load-font.R"))
 
+import_vol <- aws.s3::s3read_using(FUN = readr::read_csv,
+                                    bucket = "s3-ranch-054",
+                                    object = "theme_3/input_data/ffd_imports_volume.csv") |> 
+  mutate(area = factor(area, levels = c("eu", "non eu"), labels = c("EU", "Non EU")))
 
+chart <- 
+import_vol |> 
+  ggplot() +
+  geom_line(aes(x = year, y = value, colour = area)) +
+  geom_vline(xintercept = 2020.5, linetype = "dashed") +
+  annotate("text", x = 2020.3, y = 3e+10, label = "End of \n transition period", hjust = "right", size = 8, family = "GDS Transport Website") +
+  scale_x_continuous(breaks = seq(2010, 2023, by = 2)) +
+  scale_y_continuous(labels = scales::label_number(scale = 0.000000001), breaks = seq(0,3e+10, by = 0.6e+10), limits = c(0, 3e+10)) +
+  scale_colour_manual(values = af_colours("duo")) +
+  labs(x = NULL, y = "Million tonnes") +
+  theme_ukfsr(base_family = "GDS Transport Website")
+
+save_graphic(chart, "3.3.2a", "annual import volumes of ffd")
+save_csv(import_vol, "3.3.2a", "annual import volumes of ffd")
+
+# NOT USED import values -------------------------------------------------------
 import_data <- aws.s3::s3read_using(FUN = readr::read_csv,
                                   bucket = "s3-ranch-054",
                                   object = "theme_3/input_data/ffd_imports.csv") |> 
