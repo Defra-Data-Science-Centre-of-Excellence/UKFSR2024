29c0f45a53f206941a102c209bb005e5b84466f5
Jess Booth <Jess.Booth1@defra.gov.uk>
2024-11-13 14:12:55 +0000

    updated csv since year label was incorrect and corrected indicator numbering

--- theme_4/4.3.2 Healthy Diet.R
+++ theme_4/4.3.2 Healthy Diet.R
@@ -15,21 +15,21 @@ contents <- get_bucket_df("s3-ranch-054")
 
 # % of disposable income -------------------------------------------------------
 
-FSR_4_1_9 <- aws.s3::s3read_using(FUN = readr::read_csv,
+FSR_4_3_2d <- aws.s3::s3read_using(FUN = readr::read_csv,
                                   bucket = "s3-ranch-054",
                                   object = "theme_4/input_data/4_3_2d_perc_dispos_income.csv")
 
 
-FSR_4_1_9 <- FSR_4_1_9 %>%
-  pivot_longer(cols = c(`2020-21`, `2021-22`, `2023-24`), 
+FSR_4_3_2d <- FSR_4_3_2d %>%
+  pivot_longer(cols = c(`2020-21`, `2021-22`, `2022-23`), 
                names_to = "Year", 
                values_to = "Value")
 
-FSR_4_1_9$Year <- factor(FSR_4_1_9$Year, levels = unique(FSR_4_1_9$Year))
+FSR_4_3_2d$Year <- factor(FSR_4_3_2d$Year, levels = unique(FSR_4_3_2d$Year))
 
 
 # Plot the bar chart
-FSR_4_1_9_plot <- ggplot(FSR_4_1_9, aes(x = `Income Quintile`, y = Value, fill = Year)) +
+FSR_4_3_2d_plot <- ggplot(FSR_4_3_2d, aes(x = `Income Quintile`, y = Value, fill = Year)) +
   geom_bar(stat = "identity", position = position_dodge()) +
   geom_text(aes(label = Value), 
             position = position_dodge(width = 0.9), 
@@ -41,7 +41,7 @@ FSR_4_1_9_plot <- ggplot(FSR_4_1_9, aes(x = `Income Quintile`, y = Value, fill =
        fill = "Type") +
   theme_ukfsr() 
 
-FSR_4_1_9_plot
+FSR_4_3_2d_plot
 
-save_graphic(FSR_4_1_9_plot, '4.3.2d','Percentage of disposable income') + 
-  save_csv(FSR_4_1_9, '4.3.2d','Percentage of disposable income')
+save_graphic(FSR_4_3_2d_plot, '4.3.2d','Percentage of disposable income') + 
+  save_csv(FSR_4_3_2d, '4.3.2d','Percentage of disposable income')
